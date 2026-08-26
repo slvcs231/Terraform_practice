6e950b6403d083940d9bd2808e23c9d39130249f
Reddeiah <reddeiah18g@gmail.com>
2026-08-26 14:24:17 +0530

    adding file

---
+++ Count_instances/ec2.tf
@@ -0,0 +1,33 @@
+resource "aws_instance" "terraform" {
+  count = 3
+  ami = "ami-0220d79f3f480ecf5"
+  instance_type = "t3.micro"
+
+  tags = {
+    Name = var.instances[count.index]
+    Terraform = "True"
+  }
+}
+
+resource "aws_security_group" "allow_all" {
+  name        = "allow_all"
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+  
+  tags = {
+     Name = "allow-all"
+    }
+
+}
